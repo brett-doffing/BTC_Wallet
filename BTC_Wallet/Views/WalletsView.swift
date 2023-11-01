f608d4b73accf8057e585302f7032e2d7ccadd4d
doffing.brett <doffing.brett@gmail.com>
2023-11-01 16:25:14 -0500

    Add alert and viewModel

--- BTC_Wallet/Views/WalletsView.swift
+++ BTC_Wallet/Views/WalletsView.swift
@@ -3,7 +3,9 @@
 import SwiftUI
 
 struct WalletsView: View {
+    @StateObject var viewModel = WalletsViewModel()
     @Binding var tabSelection: Int
+
     var body: some View {
         NavigationView {
             List {
@@ -16,6 +18,16 @@ struct WalletsView: View {
                 }
             }
         }
+        .onAppear { viewModel.checkForMnemonic() }
+        .alert("Create Mnemonic", isPresented: $viewModel.needsMnemonic,
+            actions: {
+                Button("Enter Seed Words", action: { self.tabSelection = 4 })
+                Button("Randomly Generate", action: { viewModel.randomlyGenerateSeed() })
+            },
+            message: {
+                Text("How would you like to create a seed for your wallet?")
+            }
+        )
     }
 
     func callback() {
