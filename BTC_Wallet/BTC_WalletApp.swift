887068db3a72ef37ae0c6728e190a637c1ffccd4
doffing.brett <doffing.brett@gmail.com>
2023-11-01 16:36:04 -0500

    Initialize singleton for performance

--- BTC_Wallet/BTC_WalletApp.swift
+++ BTC_Wallet/BTC_WalletApp.swift
@@ -4,6 +4,12 @@ import SwiftUI
 
 @main
 struct BTC_WalletApp: App {
+
+    init() {
+        // Initialize context for performance
+        _ = BTCCurve.shared
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
