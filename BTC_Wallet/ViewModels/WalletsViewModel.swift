8212f9dc8848545fde9d1a35bdfe3a0f1404ccc1
doffing.brett <doffing.brett@gmail.com>
2023-11-07 13:19:34 -0600

    Remove checking for mnemonic

--- BTC_Wallet/ViewModels/WalletsViewModel.swift
+++ BTC_Wallet/ViewModels/WalletsViewModel.swift
@@ -4,14 +4,5 @@ import Foundation
 
 @MainActor class WalletsViewModel: ObservableObject {
     @Published var keychain: BTCKeychain?
-    @Published var needsMnemonic = false
-
-    func checkForMnemonic() {
-        if let mnemonic = UserDefaults.standard.string(forKey: "mnemonic") {
-            needsMnemonic = false
-            let seed = Mnemonic.createSeed(mnemonic: mnemonic)
-        } else {
-            needsMnemonic = true
-        }
-    }
+    @Published var showMnemonic = false
 }
