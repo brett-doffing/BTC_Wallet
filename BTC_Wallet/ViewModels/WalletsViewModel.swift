2226bea3995fa341ac4a0d6f98ea072720e5ec99
doffing.brett <doffing.brett@gmail.com>
2023-11-08 17:10:19 -0600

    Update WalletsViewModel.swift

--- BTC_Wallet/ViewModels/WalletsViewModel.swift
+++ BTC_Wallet/ViewModels/WalletsViewModel.swift
@@ -5,4 +5,14 @@ import Foundation
 @MainActor class WalletsViewModel: ObservableObject {
     @Published var keychain: BTCKeychain?
     @Published var showMnemonic = false
+    @Published var showNameAlert = false
+    @Published var wallets: [[String: String]] = []
+    @Published var walletName = ""
+
+    func getWallets() {
+        if let savedWallets = UserDefaults.standard.object(forKey: "wallets") as? [[String: String]] {
+            print(savedWallets)
+            wallets = savedWallets
+        }
+    }
 }
