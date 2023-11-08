88f1c342c1fda5765d549681f906e9985d0fca6f
doffing.brett <doffing.brett@gmail.com>
2023-11-08 17:11:30 -0600

    Add alert for adding a wallet name, and show multiple wallets

--- BTC_Wallet/Views/WalletsView.swift
+++ BTC_Wallet/Views/WalletsView.swift
@@ -12,21 +12,31 @@ struct WalletsView: View {
                     Section(
                         header: SectionHeaderView(
                             heading: "Wallets",
-                            callback: showMnemonic
+                            callback: { viewModel.showNameAlert = true }
                         )
                     ) {
-
+                        ForEach($viewModel.wallets, id: \.self) { wallet in
+                            if let name = wallet.wrappedValue["name"] {
+                                Text(name)
+                            }
+                        }
                     }
                 }
                 .listStyle(.insetGrouped)
             }
+            .onAppear { viewModel.getWallets() }
             .navigationDestination(isPresented: $viewModel.showMnemonic) {
-                MnemonicView()
+                let vm = MnemonicViewModel(viewModel.walletName)
+                MnemonicView(viewModel: vm)
+            }
+            .alert("Wallet Name", isPresented: $viewModel.showNameAlert) {
+                TextField("WalletName", text: $viewModel.walletName)
+                Button("OK", action: { saveName() })
             }
         }
     }
 
-    func showMnemonic() {
+    func saveName() {
         viewModel.showMnemonic = true
     }
 }
