98043ef97afb61420102ad9efcd1980ca9d17d33
doffing.brett <doffing.brett@gmail.com>
2023-11-08 17:10:09 -0600

    Refactor to allow for multiple wallets

--- BTC_Wallet/ViewModels/MnemonicViewModel.swift
+++ BTC_Wallet/ViewModels/MnemonicViewModel.swift
@@ -2,60 +2,68 @@
 
 import Foundation
 
-extension MnemonicView {
-    @MainActor class MnemonicViewModel: ObservableObject {
-        @Published var words: [String] = []
-        @Published var hasMnemonic = false
-        @Published var shouldQuiz = false
+class MnemonicViewModel: ObservableObject {
+    @Published var words = [String](repeating: "", count: 12)
+    @Published var hasMnemonic = false
+    @Published var shouldQuiz = false
+    var wallets: [[String: String]] = []
+    var walletName: String?
 
-        init(words: [String]? = nil) {
-            checkForMnemonic()
-        }
+    init(_ walletName: String? = nil) {
+        self.walletName = walletName
+        checkForMnemonic()
+    }
 
-        func checkForMnemonic() {
-            if let mnemonic = UserDefaults.standard.object(forKey: "mnemonic") as? String {
-                words = mnemonic.components(separatedBy: " ")
-                hasMnemonic = true
-            } else {
-                words = [String](repeating: "", count: 12)
-            }
-        }
-
-        func saveWord(word: String, index: Int) {
-            words[index] = word
-        }
-
-        func saveMnemonic() {
-            if validateMnemonic() {
-                let mnemonic = words.map { $0.lowercased() }.joined(separator: " ")
-                UserDefaults.standard.set(mnemonic, forKey: "mnemonic")
-                shouldQuiz = true
-            }
-        }
-
-        private func validateMnemonic() -> Bool {
-            for word in words {
-                if !WordList.english.words.contains(word) {
-                    return false
+    func checkForMnemonic() {
+        if let wallets = UserDefaults.standard.object(forKey: "wallets") as? [[String: String]] {
+            self.wallets = wallets
+            for wallet in wallets {
+                if let name = wallet["name"], name == walletName, let mnemonic = wallet["mnemonic"] {
+                    words = mnemonic.components(separatedBy: " ")
+                    hasMnemonic = true
                 }
             }
-            return true
         }
+    }
 
-        func randomlyGenerateSeed() {
-            let mnemonic = Mnemonic.createRandom()
+    func saveWord(word: String, index: Int) {
+        words[index] = word
+    }
 
-            // TODO: Save Securely
+    func saveMnemonic() {
+        if validateMnemonic() {
+            let mnemonic = words.map { $0.lowercased() }.joined(separator: " ")
             UserDefaults.standard.set(mnemonic, forKey: "mnemonic")
-    //            let kcpi = KeychainPasswordItem(service: "wallet", account: "satoshi")
-    //            do { try kcpi.savePassword(mnemonic) }
-    //            catch let kcError {
-    //                print("error = \(kcError)")
-    //                isLoading = false
-    //                return
-    //            }
-            checkForMnemonic()
             shouldQuiz = true
         }
+    }
+
+    private func validateMnemonic() -> Bool {
+        for word in words {
+            if !WordList.english.words.contains(word) {
+                return false
+            }
+        }
+        return true
+    }
+
+    func randomlyGenerateSeed() {
+        let mnemonic = Mnemonic.createRandom()
+
+        if let name = walletName {
+            wallets += [["name": name, "mnemonic": mnemonic]]
+            UserDefaults.standard.set(wallets, forKey: "wallets")
+        }
+
+        // TODO: Save Securely
+//            let kcpi = KeychainPasswordItem(service: "wallet", account: "satoshi")
+//            do { try kcpi.savePassword(mnemonic) }
+//            catch let kcError {
+//                print("error = \(kcError)")
+//                isLoading = false
+//                return
+//            }
+        checkForMnemonic()
+        shouldQuiz = true
     }
 }
