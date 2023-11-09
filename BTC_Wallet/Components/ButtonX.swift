77d63202bb0c62023146f58dffab74d1da42467d
doffing.brett <doffing.brett@gmail.com>
2023-11-09 10:32:50 -0600

    Add `ButtonX` component

---
+++ BTC_Wallet/Components/ButtonX.swift
@@ -0,0 +1,21 @@
+// ButtonX.swift
+
+import SwiftUI
+
+struct ButtonX: View {
+    var text: String
+    var callback: (() -> Void) /// use closure for callback
+
+    var body: some View {
+        Button(action: callback) {
+            Text(text)
+        }
+        .frame(width: 150, height: 50)
+        .cornerRadius(10)
+        .overlay {
+            RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color("btcOrange"), lineWidth: 5)
+                    .allowsHitTesting(false)
+        }
+    }
+}
--- BTC_Wallet/Views/QuizView.swift
+++ BTC_Wallet/Views/QuizView.swift
@@ -10,50 +10,30 @@ struct QuizView: View {
         VStack {
             Text("What is word #\(viewModel.wordIndex + 1)")
                 .padding()
-            Button("\(viewModel.word1)") {
+            ButtonX(text: "\(viewModel.word1)") {
                 viewModel.selected(index: 1) { dismiss in
-                    if dismiss {
-                        presentationMode.wrappedValue.dismiss()
-                    }
+                    if dismiss { dismissView() }
                 }
-            }
-            .frame(width: 150, height: 50)
-            .cornerRadius(10)
-            .overlay {
-                RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color("btcOrange"), lineWidth: 5)
             }
             .padding()
 
-            Button("\(viewModel.word2)") {
+            ButtonX(text: "\(viewModel.word2)") {
                 viewModel.selected(index: 2) { dismiss in
-                    if dismiss {
-                        presentationMode.wrappedValue.dismiss()
-                    }
+                    if dismiss { dismissView() }
                 }
-            }
-            .frame(width: 150, height: 50)
-            .cornerRadius(10)
-            .overlay {
-                RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color("btcOrange"), lineWidth: 5)
             }
             .padding()
             
-            Button("\(viewModel.word3)") {
+            ButtonX(text: "\(viewModel.word3)") {
                 viewModel.selected(index: 3) { dismiss in
-                    if dismiss {
-                        presentationMode.wrappedValue.dismiss()
-                    }
+                    if dismiss { dismissView() }
                 }
-            }
-            .frame(width: 150, height: 50)
-            .cornerRadius(10)
-            .overlay {
-                RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color("btcOrange"), lineWidth: 5)
             }
             .padding()
         }
+    }
+
+    private func dismissView() {
+        presentationMode.wrappedValue.dismiss()
     }
 }
