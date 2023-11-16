9d04041b54c6d19dacc4bb7393a48f9f841c7d76
doffing.brett <doffing.brett@gmail.com>
2023-11-16 14:55:22 -0600

    Cleanup and Documentation

--- BTC_Wallet/ViewModels/QuizViewModel.swift
+++ BTC_Wallet/ViewModels/QuizViewModel.swift
@@ -4,8 +4,7 @@ import Foundation
 
 @MainActor class QuizViewModel: ObservableObject {
     let words: [String]
-    /// Holds indices of question that have already been asked
-    var wordIndices: [Int] = []
+    var wordIndices: [Int] = [] /// Holds indices of questions that have already been asked
     var answerButtonNumber = 0
     var questionNumber = 1
     let dismissMnemonicView: (Bool) -> ()
@@ -25,15 +24,38 @@ import Foundation
      */
     private func generateQuestion() {
         /// Index for answer to question
-        repeat {
-            wordIndex = Int.random(in: 0..<12)
-        } while wordIndices.contains(wordIndex)
+        wordIndex = getNewWordIndex()
         wordIndices.append(wordIndex)
 
-        /// Random button will be the answer
-        answerButtonNumber = Int.random(in: 1...3)
+        generateAnswerOptions()
 
-        /// Indices that shouldn't be repeated as possible answer options
+        #if DEBUG
+        print(words[wordIndex]) // answer
+        #endif
+    }
+
+    /**
+     Randomly selects a new word, while also making sure it has not been used previously.
+
+     - Returns: The random int to index the `words` array
+     */
+    private func getNewWordIndex() -> Int {
+        var answerIndex: Int
+        repeat {
+            answerIndex = Int.random(in: 0..<12)
+        } while wordIndices.contains(answerIndex)
+
+        return answerIndex
+    }
+
+    /**
+     Randomly selects which button will be the answer,
+     while ensuring that no other buttons can be the answer.
+     */
+    private func generateAnswerOptions() {
+        /// Select random button that will be the answer
+        answerButtonNumber = Int.random(in: 1...3)
+        /// Indices that shouldn't be repeated as possible options for answers
         var answerIndices: [Int] = []
 
         for i in 1...3 {
@@ -56,9 +78,6 @@ import Foundation
                 word3 = words[idx]
             }
         }
-        #if DEBUG
-        print(words[wordIndex]) // answer
-        #endif
     }
 
     /**
