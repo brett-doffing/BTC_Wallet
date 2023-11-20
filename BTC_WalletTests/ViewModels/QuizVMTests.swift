0d51eef72f192230a11c6eb27001a098f3f4078c
doffing.brett <doffing.brett@gmail.com>
2023-11-20 12:16:40 -0600

    Test twelve words

--- BTC_WalletTests/ViewModels/QuizVMTests.swift
+++ BTC_WalletTests/ViewModels/QuizVMTests.swift
@@ -25,7 +25,7 @@ final class QuizVMTests: XCTestCase {
         XCTAssertTrue(vm.wordIndex == -1)
     }
 
-    func test_QuizViewModel_words_partial() {
+    func test_QuizViewModel_words_sixWords() {
         // Given
         let words: [String] = ["some", "words", "exist", "but", "not", "enough"]
         // When
@@ -37,4 +37,15 @@ final class QuizVMTests: XCTestCase {
         XCTAssertTrue(vm.wordIndex == -1)
     }
 
+    func test_QuizViewModel_words_twelveWords() {
+        // Given
+        let words: [String] = ["legal", "winner", "thank", "year", "wave", "sausage", "worth", "useful", "legal", "winner", "thank", "yellow"]
+        // When
+        let vm = QuizViewModel(words: words, dismissMnemonicView: {_ in })
+        // Then
+        XCTAssertTrue(vm.word1 != "")
+        XCTAssertTrue(vm.word2 != "")
+        XCTAssertTrue(vm.word3 != "")
+        XCTAssertTrue(vm.wordIndex != -1)
+    }
 }
