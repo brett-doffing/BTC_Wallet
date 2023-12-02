5d65e5e9f3a1c5dd0c24d05b073259a626b367ab
doffing.brett <doffing.brett@gmail.com>
2023-12-02 13:46:51 -0600

    Update BlockstreamService.swift

--- BTC_Wallet/Networking/BlockstreamService.swift
+++ BTC_Wallet/Networking/BlockstreamService.swift
@@ -10,23 +10,16 @@ class BlockstreamService {
 
     private init() {}
 
-    private func getRequest(withURL url: URL, completionHandler: @escaping (Data?, Error?) -> ()) {
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if error != nil { completionHandler(nil, error) }
-            guard let data = data else { completionHandler(nil, error); return }
-            completionHandler(data, nil)
-        }.resume()
-    }
-
-    func getTransactions(for address: String, lastSeenTX: String? = nil) async {
+    func getTransactions(for address: String, lastSeenTX: String? = nil) async throws -> [BlockstreamResponse] {
         var urlString = baseURL + "address/\(address)/txs"
         if let lastSeenTX = lastSeenTX { urlString += "/chain/\(lastSeenTX)"}
-        guard let url = URL(string: urlString) else { return }
+        guard let url = URL(string: urlString) else { throw "Bad URL when fetching a BlockstreamResponse" }
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            let transactions = try JSONDecoder().decode([String: String].self, from: data)
+            let transactions = try JSONDecoder().decode([BlockstreamResponse].self, from: data)
+            return transactions
         } catch {
-            print(error)
+            throw "BlockstreamResponse Error"
         }
     }
 }
