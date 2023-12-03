cc41507645668167070f2ac3abbf026aa27ac4b7
doffing.brett <doffing.brett@gmail.com>
2023-12-03 16:33:39 -0600

    Remove singleton, add protocol

--- BTC_Wallet/Networking/BlockstreamService.swift
+++ BTC_Wallet/Networking/BlockstreamService.swift
@@ -2,17 +2,17 @@
 
 import Foundation
 
-class BlockstreamService {
-    static let shared = BlockstreamService()
+protocol BlockstreamService {
+    func getTransactions(for address: String, lastSeenTX: String?) async throws -> [BlockstreamResponse]
+}
 
-    let baseURL = "https://blockstream.info/testnet/api/"
-    let defaults = UserDefaults.standard
-
-    private init() {}
+struct _BlockstreamService: BlockstreamService {
+    private let baseURL = "https://blockstream.info/testnet/api/"
+    private let defaults = UserDefaults.standard
 
     func getTransactions(for address: String, lastSeenTX: String? = nil) async throws -> [BlockstreamResponse] {
         var urlString = baseURL + "address/\(address)/txs"
-        if let lastSeenTX = lastSeenTX { urlString += "/chain/\(lastSeenTX)"}
+        if let lastSeenTX = lastSeenTX { urlString += "/chain/\(lastSeenTX)" }
         guard let url = URL(string: urlString) else { throw "Bad URL when fetching a BlockstreamResponse" }
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
