c52e7dda4e6ad836e327232bdd2f1283881fc543
doffing.brett <doffing.brett@gmail.com>
2023-12-04 17:10:48 -0600

    Initial wallet

--- BTC_Wallet/ViewModels/WalletViewModel.swift
+++ BTC_Wallet/ViewModels/WalletViewModel.swift
@@ -7,10 +7,29 @@ import Foundation
     @Published var address: String?
     @Published var isLoading = false
     @Published var transactions: [TX] = []
+    private let service = BlockstreamService()
 
     let wallet: Wallet
 
     init(_ wallet: Wallet) {
         self.wallet = wallet
+        guard let mnemonic = wallet.mnemonic else { return }
+        let seed = Mnemonic.createSeed(mnemonic: mnemonic)
+        let masterKeychain = BTCKeychain(seed: seed)
+        let coinType = masterKeychain.network.coinType
+        if let kc = masterKeychain.derivedKeychain(withPath: "m/84'/\(coinType)'/0'", andType: .BIP84),
+           let address = kc.recieveKeychain(atIndex: UInt32(wallet.walletIndex), withType: .BIP84)?.address
+        {
+            self.keychain = kc
+            self.address = address
+//            getTXs(forAddress: address, isCurrentAddress: true)
+        }
+    }
+
+    func refresh() {
+//        checkUnconfirmedTXs()
+//        if let address = self.address {
+//            getTXs(forAddress: address)
+//        }
     }
 }
--- BTC_Wallet/Views/WalletView.swift
+++ BTC_Wallet/Views/WalletView.swift
@@ -1,12 +1,106 @@
 // WalletView.swift
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
-//struct WalletView: View {
-//    @StateObject var viewModel = WalletViewModel()
-//
-//    var body: some View {
-//        Text("Hello, Wallet!")
-//    }
-//}
+struct WalletView: View {
+    @StateObject var viewModel: WalletViewModel
+
+    @State private var copied = false {
+        didSet {
+            if copied == true {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation { copied = false }
+                }
+            }
+        }
+    }
+
+    let qrContext = CIContext()
+    let qrFilter = CIFilter.qrCodeGenerator()
+
+    var body: some View {
+        NavigationView {
+            GeometryReader { geometry in
+                ZStack {
+                    VStack {
+                        Image(uiImage: generateQRCode())
+                            .resizable()
+                            .interpolation(.none) // Do not smooth
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .padding()
+                        if let address = viewModel.address {
+                            Text("\(address) \(Image(systemName: "doc.on.doc"))")
+                                .padding()
+                                .onTapGesture() {
+                                    UIPasteboard.general.string = address
+                                    withAnimation { copied = true }
+                                }
+                        } else {
+                            Text("").padding()
+                        }
+                        List {
+                            Section(header: SectionHeaderView(heading: "Transactions")) {
+//                                ForEach($viewModel.transactions) { $tx in
+//                                    let viewModel = TransactionViewModel(model: tx)
+//                                    NavigationLink {
+//                                        TransactionFullView(viewModel: viewModel)
+//                                    } label: {
+//                                        TransactionListView(viewModel: viewModel)
+//                                    }
+//                                }
+                            }
+                        }
+                        .listStyle(.insetGrouped)
+                        .refreshable {
+                            viewModel.refresh()
+                        }
+                        Spacer()
+                    }
+                    if viewModel.isLoading {
+                        ProgressView()
+                            .scaleEffect(3)
+                    }
+                    if copied {
+                        Text("Copied to clipboard")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color("btcOrange").cornerRadius(10))
+                            .position(x: geometry.frame(in: .local).width/2)
+                            .transition(.move(edge: .top))
+                            .padding(.top)
+                            .animation(Animation.easeInOut(duration: 0.5))
+                    }
+                }
+            }
+        }
+//        .onAppear { viewModel.checkForWallet() }
+//        .alert("Create Wallet", isPresented: $viewModel.needsWallet,
+//            actions: {
+//                Button("Enter Seed Words", action: { self.tabSelection = 4 })
+//                Button("Randomly Generate", action: { viewModel.randomlyGenerateSeed() })
+//            },
+//            message: {
+//                Text("How would you like to create a seed for your wallet?")
+//            }
+//        )
+    }
+
+    private func generateQRCode() -> UIImage {
+        guard let address = viewModel.address else {
+            return UIImage()
+        }
+        qrFilter.message = Data(address.utf8)
+
+        if let outputImage = qrFilter.outputImage,
+           let cgImg = qrContext.createCGImage(outputImage, from: outputImage.extent)
+        {
+            return UIImage(cgImage: cgImg)
+        }
+
+        return UIImage()
+    }
+}
 
