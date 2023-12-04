a7a565bc8177c6d99faa2fa99bcfc77d581a4f97
doffing.brett <doffing.brett@gmail.com>
2023-12-04 09:04:25 -0600

    Add `Codable`, `Identifiable`, and properties

--- BTC_Wallet/Models/Wallet.swift
+++ BTC_Wallet/Models/Wallet.swift
@@ -2,8 +2,13 @@
 
 import Foundation
 
-struct Wallet {
-    var changeIndex: Int
-    var walletIndex: Int
-    var transactions: [TX]
+struct Wallet: Codable, Identifiable {
+    var name: String
+    var changeIndex = 0
+    var walletIndex = 0
+    /// Holds `[TX.id]` for reference, instead of entire TX
+    var transactions: [String] = []
+    var mnemonic: String?
+
+    var id: String { self.name }
 }
