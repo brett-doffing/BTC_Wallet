b86fcc6cb73c831154e6700929bd4630afde4938
doffing.brett <doffing.brett@gmail.com>
2023-12-05 16:16:20 -0600

    Remove functions

--- BTC_Wallet/Models/V_out.swift
+++ BTC_Wallet/Models/V_out.swift
@@ -8,16 +8,11 @@ struct V_out: Codable {
     var scriptpubkey_address: String?
     var scriptpubkey_type: String?
     var value: Double
-    var n: Int?
 
     /// Non API
+    var n: Int? // Sequence number
     var isTXO: Bool? // Flag wallet TXO
     var isChange: Bool? // Denotes wallet change
     var isSpent: Bool? // Unspent (U)TXO
     var txid: String? // Used for TX creation?
-
-    mutating func toggleIsTXO() { self.isTXO = true }
-    mutating func toggleIsChange() { self.isChange = true }
-    mutating func toggleIsSpent() { self.isSpent = true }
-    mutating func setTXID(_ txid: String) { self.txid = txid }
 }
