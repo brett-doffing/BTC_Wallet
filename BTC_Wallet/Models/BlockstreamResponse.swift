05d0458c6e687beddfd906b1a385f3791d250c28
doffing.brett <doffing.brett@gmail.com>
2023-12-05 16:11:50 -0600

    Map from response to `TX`

--- BTC_Wallet/Models/BlockstreamResponse.swift
+++ BTC_Wallet/Models/BlockstreamResponse.swift
@@ -16,59 +16,19 @@ struct BlockstreamResponse: Codable, Identifiable {
 
     var id: String { txid }
 
-//    func mapped(viewContext: NSManagedObjectContext) -> TX {
-//        /// Initialize this way, instead of TX(context:) to silence ambiguous instances warning
-//        let newTX = NSEntityDescription.insertNewObject(
-//            forEntityName: "TX",
-//            into: viewContext
-//        ) as! TX
-//
-//        newTX.id = txid
-//        newTX.fee = fee
-//        newTX.locktime = locktime
-//        newTX.size = size
-//        newTX.version = version
-//        newTX.weight = weight
-//        if let blockHeight = status.block_height,
-//           let blockTime = status.block_time
-//        {
-//            newTX.blockHeight = blockHeight
-//            newTX.blockTime = blockTime
-//        }
-//        newTX.confirmed = status.confirmed
-//        var cdVin: [V_IN] = []
-//        for vin in vin {
-//            let newVin = NSEntityDescription.insertNewObject(
-//                forEntityName: "V_IN",
-//                into: viewContext
-//            ) as! V_IN
-//
-//            newVin.isCoinbase = vin.is_coinbase
-//            newVin.scriptSig = vin.scriptsig
-//            newVin.sequence = vin.sequence
-//            newVin.txid = vin.txid
-//            newVin.vout = vin.vout
-//            newVin.witness = vin.witness
-//            cdVin.append(newVin)
-//        }
-//        newTX.v_in = NSSet(array: cdVin)
-//
-//        var cdVout: [V_OUT] = []
-//        for (i, vout) in vout.enumerated() {
-//            let newVout = NSEntityDescription.insertNewObject(
-//                forEntityName: "V_OUT",
-//                into: viewContext
-//            ) as! V_OUT
-//
-//            newVout.address = vout.scriptpubkey_address
-//            newVout.n = Int64(i)
-//            if let isWalletTXO = vout.isTXO { newVout.isWalletTXO = isWalletTXO }
-//            newVout.scriptPubKey = vout.scriptpubkey
-//            newVout.type = vout.scriptpubkey_type
-//            newVout.value = vout.value
-//            cdVout.append(newVout)
-//        }
-//        newTX.v_out = NSSet(array: cdVout)
-//        return newTX
-//    }
+    func mapped() -> TX {
+        TX(
+            blockHeight: status.block_height,
+            blockTime: status.block_time,
+            confirmed: status.confirmed,
+            fee: fee,
+            id: txid,
+            locktime: locktime,
+            size: size,
+            version: version,
+            weight: weight,
+            v_in: vin,
+            v_out: vout
+        )
+    }
 }
