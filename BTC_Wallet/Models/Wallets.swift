07f76de03a869fac075c4bdbf496e68f6f6880e4
doffing.brett <doffing.brett@gmail.com>
2023-12-05 07:50:17 -0600

    Update Wallets.swift

--- BTC_Wallet/Models/Wallets.swift
+++ BTC_Wallet/Models/Wallets.swift
@@ -6,23 +6,30 @@ class Wallets: ObservableObject {
     @Published var wallets: [Wallet] = []
 
     init() {
+        getWallets()
+    }
+
+    func getWallets() {
         let decoder = JSONDecoder()
         if let savedWallets = UserDefaults.standard.object(forKey: "wallets") as? Data,
            let wallets = try? decoder.decode([Wallet].self, from: savedWallets)
         {
             self.wallets = wallets
+        } else {
+            self.wallets = []
         }
     }
 
-    func save(_ wallets: [Wallet]) {
+    func save() {
         let encoder = JSONEncoder()
         if let encodedWallets = try? encoder.encode(wallets) {
             UserDefaults.standard.set(encodedWallets, forKey: "wallets")
         }
     }
 
-    func save() {
-        save(wallets)
+    func save(_ wallets: [Wallet]) {
+        self.wallets = wallets
+        save()
     }
 
     func save(_ wallet: Wallet) {
