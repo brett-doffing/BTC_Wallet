ee01461b202093c5483b75190dbe4782853eb960
doffing.brett <doffing.brett@gmail.com>
2023-12-09 10:28:01 -0600

    Update styling and content

--- BTC_Wallet/Components/TransactionListView.swift
+++ BTC_Wallet/Components/TransactionListView.swift
@@ -6,6 +6,10 @@ struct TransactionListView: View {
     @State var tx: TX
     var txo: V_out?
     let dateFormatter = DateFormatter()
+    var arrowImageName: String {
+        if txo?.isSpent == true { return "arrow.up.to.line" }
+        return "arrow.down.to.line"
+    }
 
     init(for tx: TX) {
         self.tx = tx
@@ -14,11 +18,21 @@ struct TransactionListView: View {
 
     var body: some View {
         HStack {
-            Text(blockTime)
-            Spacer()
-            Text("\(Int(txo?.value ?? 0)) Sats")
+            Image(systemName: arrowImageName)
+                .imageScale(.large)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(txo?.isSpent == true ? "Sent:" : "Received:")
+                    Text(blockTime)
+                        .font(.callout)
+                        .foregroundColor(txo?.isSpent == true ? .red : .gray)
+                }
+                Spacer()
+                Text("\(Int(txo?.value ?? 0)) Sats")
+                    .minimumScaleFactor(0.1)
+            }
         }
-        .frame(height: 30)
+        .font(.headline)
     }
 
     var blockTime: String {
@@ -44,8 +58,11 @@ struct TransactionListView: View {
 }
 
 
-//struct TransactionListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TransactionListView()
-//    }
-//}
+struct TransactionListView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TransactionListView(for: PreviewMocks.tx1)
+            TransactionListView(for: PreviewMocks.tx2)
+        }
+    }
+}
