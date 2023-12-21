0285cd97a8ea724d0c1e30ad5d8dc054dc35b473
doffing.brett <doffing.brett@gmail.com>
2023-12-21 12:14:04 -0600

    Update DataStore.swift

--- BTC_Wallet/DataStore.swift
+++ BTC_Wallet/DataStore.swift
@@ -1,3 +1,37 @@
 // DataStore.swift
 
 import Foundation
+
+class DataStore {
+    static let shared = DataStore()
+
+    private init() {
+        load()
+    }
+
+    var wallets: [Wallet] = []
+    var currentWallet: Wallet?
+
+    func save() {
+        do {
+            let fileURL = URL.documentsDirectory.appending(path: "Wallets.json")
+            let walletsData = try JSONEncoder().encode(wallets)
+            try walletsData.write(to: fileURL)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func load() {
+        let fileURL = URL.documentsDirectory.appending(path: "Wallets.json")
+        if FileManager().fileExists(atPath: fileURL.path) {
+            do {
+                let walletsData = try Data(contentsOf: fileURL)
+                wallets = try JSONDecoder().decode([Wallet].self, from: walletsData)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+
+    }
+}
