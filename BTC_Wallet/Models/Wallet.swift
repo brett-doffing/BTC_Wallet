30ca47d0dfe73aa5f961a0bbd4acfb6c9144042b
doffing.brett <doffing.brett@gmail.com>
2023-12-22 08:55:05 -0600

    Update model

--- BTC_Wallet/Models/Wallet.swift
+++ BTC_Wallet/Models/Wallet.swift
@@ -3,12 +3,26 @@
 import Foundation
 
 struct Wallet: Codable, Identifiable {
-    var name: String = "New Wallet"
+    var id = UUID()
+    var name = "New Wallet"
     var changeIndex = 0
     var walletIndex = 0
-    /// Holds `[TX.id]` for reference, instead of entire TX
-    var transactions: [String] = []
+    var transactions: [TX] = []
     var mnemonic: String?
 
-    var id: String { self.name }
+    var keychain: BTCKeychain? {
+        guard let mnemonic else { return nil }
+        let seed = Mnemonic.createSeed(mnemonic: mnemonic)
+        let masterKeychain = BTCKeychain(seed: seed)
+        let coinType = masterKeychain.network.coinType // Can change?
+        return masterKeychain.derivedKeychain(withPath: "m/44'/\(coinType)'/0'", andType: .BIP44)
+    }
+    var receiveKeychain: BTCKeychain? {
+        return keychain?.receiveKeychain(atIndex: UInt32(walletIndex), withType: .BIP44)
+    }
+    var changeKeychain: BTCKeychain? {
+        return keychain?.changeKeychain(atIndex: UInt32(changeIndex), withType: .BIP44)
+    }
+    var receiveAddress: String? { receiveKeychain?.address }
+    var changeAddress: String? { changeKeychain?.address }
 }
