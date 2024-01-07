eb0a302f38346537d0fed10d8a146d2272e51d78
doffing.brett <doffing.brett@gmail.com>
2024-01-07 16:06:39 -0600

    Add Sections

--- BTC_Wallet/Views/SendTransactionView.swift
+++ BTC_Wallet/Views/SendTransactionView.swift
@@ -8,13 +8,35 @@ struct SendTransactionView: View {
 
     var body: some View {
         VStack {
-            if let tx = transaction?.rawTX?.hexDescription() {
-                Text(tx)
-                    .padding()
+            if let transaction {
+                List {
+                    Section("Send:") {
+                        Text("\(transaction.receiverAmounts[0]) Satoshis")
+                    }
+                    Section("To:") {
+                        Text(transaction.receivingAddresses[0])
+                    }
+                    Section("Fee:") {
+                        Text("\(transaction.fee) Satoshis")
+                        Text("\(transaction.feePerVByte) sats/vByte")
+                    }
+                    Section("Total:") {
+                        Text("\(transaction.fee + Int(transaction.receiverAmounts[0])) Satoshis")
+                    }
+                }
             }
+            Spacer()
             SliderLock(unlocked: $canSend, title: "Slide to Send")
                 .frame(height: 50)
                 .padding()
+        }
+        .onChange(of: canSend) { newValue in
+            if canSend, let tx = transaction?.rawTX {
+                print(tx)
+                print("HEX Count: \(tx.hexDescription().count)")
+                print("BYTES Count: \(tx.bytes.count)")
+                canSend = false
+            }
         }
     }
 }
